a1f2b8ef37434ec5eaadcd31e18beba290204190
Ankur Mallick <ankurmallick80@gmail.com>
2024-09-22 00:05:13 +0530

    minor update

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,26 @@
 use std::{env, fs, io};
+use io::Write;
 
 macro_rules! printf {
     ($($arg:tt)*) => {{
-        use std::io::Write;
         print!($($arg)*);
-        std::io::stdout().flush().expect("ERROR: Couldn't flush to output stream.");
+        io::stdout().flush().expect("ERROR: Couldn't flush to output stream.");
     }};
 }
 
-/// parse_parentheses(file, level, start, end, index)
+/// parse_parentheses(file, start, end, index)
 macro_rules! parse_parentheses {
-    ($file: ident, $level: ident, $start: literal, $end: literal ,$index: ident) => {{
+    ($file: ident, $start: literal, $end: literal ,$index: ident) => {{
+        let mut level = 0;
+        
         loop {
             let char = $file.chars().nth($index).expect("ERROR: Tried to get character at invalid index.");
             match char {
-                $start => $level += 1,
-                $end   => $level -= 1,
+                $start => level += 1,
+                $end   => level -= 1,
                 _   => ()
             }
-            if $level==0 {
+            if level==0 {
                 break;
             }
             match $start=='[' { 
@@ -41,16 +43,22 @@ fn main() {
             Ok(file) => file,
             Err(e)    =>  panic!("ERROR: {e}")
         };
-
-    let mut mem: [u8; 30000] = [0; 30000];
+    
+    const CELL_SIZE: usize = 30000;
+    let mut mem: [u8; CELL_SIZE] = [0; CELL_SIZE];
     let (mut index, mut i) = (0, 0);
     
     while i<file.len() {
         match file.chars().nth(i).expect("ERROR: Tried to get character at invalid index.") {
-            '>' => index += 1,
+            '>' => {
+                if index==CELL_SIZE {
+                    panic!("ERROR: Memory out of bounds (overflow), exceeded the memory size of {CELL_SIZE} cells.");
+                }
+                index += 1
+            },
             '<' => {
                 if index==0 {
-                    panic!("ERROR: Memory underflow!");
+                    panic!("ERROR: Memory out of bounds (underflow), undershot the memory size of {CELL_SIZE} cells.");
                 }
                 index -= 1;
             },
@@ -65,17 +73,15 @@ fn main() {
                 mem[index] = inp.chars().nth(0).expect("ERROR: Tried to get character at invalid index.") as u8;
             }
             '[' => {
-                let mut level = 0;
                 match mem[index] {
-                    0 => parse_parentheses!(file, level, '[', ']', i),
+                    0 => parse_parentheses!(file, '[', ']', i),
                     _ => ()
                 }
             }
             ']' => {
-                let mut level = 0;
                 match mem[index] {
                     0 => (),
-                    _ => parse_parentheses!(file, level, ']', '[', i)
+                    _ => parse_parentheses!(file, ']', '[', i)
                 }
             }
             _ => ()
