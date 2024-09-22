af575f90aa5bea68c118f9342d036c19b14303ee
Ankur Mallick <ankurmallick80@gmail.com>
2024-09-22 17:14:10 +0530

    add additional flags and fix newline take

--- src/main.rs
+++ src/main.rs
@@ -39,54 +39,70 @@ fn main() {
         _ => ()
     }
 
-    let file = match fs::read_to_string(&args[1]) {
-            Ok(file) => file,
-            Err(e)    =>  panic!("ERROR: {e}")
-        };
-    
-    const CELL_SIZE: usize = 30000;
-    let mut mem: [u8; CELL_SIZE] = [0; CELL_SIZE];
-    let (mut index, mut i) = (0, 0);
-    
-    while i<file.len() {
-        match file.chars().nth(i).expect("ERROR: Tried to get character at invalid index.") {
-            '>' => {
-                if index==CELL_SIZE {
-                    panic!("ERROR: Memory out of bounds (overflow), exceeded the memory size of {CELL_SIZE} cells.");
-                }
-                index += 1
-            },
-            '<' => {
-                if index==0 {
-                    panic!("ERROR: Memory out of bounds (underflow), undershot the memory size of {CELL_SIZE} cells.");
-                }
-                index -= 1;
-            },
-            '+' => mem[index] += 1,
-            '-' => mem[index] -= 1,
-            '.' => printf!("{}", mem[index] as char),
-            ',' => {
-                let mut inp = String::new();
-                io::stdin().read_line(&mut inp).expect("ERROR: Failed to read line.");
+    match args[1].as_str() {
+        "--version" | "-v" => {
+            println!("Brainfuck compiler, bfc v0.0.3");
+            println!("Copyright (C) 2024 Ankur Mallick.");
+        },
 
-                let inp = inp.trim();
-                mem[index] = inp.chars().nth(0).expect("ERROR: Tried to get character at invalid index.") as u8;
-            }
-            '[' => {
-                match mem[index] {
-                    0 => parse_parentheses!(file, '[', ']', i),
+        "--help" | "-h" => println!("Usage: bfc <file>"),
+
+        file_name if file_name.ends_with(".bf") || file_name.ends_with(".b") => {
+            let file = match fs::read_to_string(file_name) {
+                Ok(file) => file,
+                Err(e)    =>  panic!("ERROR: {e}")
+            };
+
+            const CELL_SIZE: usize = 30000;
+            let mut mem: [u8; CELL_SIZE] = [0; CELL_SIZE];
+            let (mut index, mut i) = (0, 0);
+
+            while i<file.len() {
+                match file.chars().nth(i).expect("ERROR: Tried to get character at invalid index.") {
+                    '>' => {
+                        if index==CELL_SIZE {
+                            panic!("ERROR: Memory out of bounds (overflow), exceeded the memory size of {CELL_SIZE} cells.");
+                        }
+                        index += 1
+                    },
+                    '<' => {
+                        if index==0 {
+                            panic!("ERROR: Memory out of bounds (underflow), undershot the memory size of {CELL_SIZE} cells.");
+                        }
+                        index -= 1;
+                    },
+                    '+' => mem[index] += 1,
+                    '-' => mem[index] -= 1,
+                    '.' => printf!("{}", mem[index] as char),
+                    ',' => {
+                        let mut inp = String::new();
+                        io::stdin().read_line(&mut inp).expect("ERROR: Failed to read line.");
+
+                        let mut ascii = inp.chars().nth(0).expect("ERROR: No input was given.") as u8;
+                        if ascii == 13 {
+                            ascii = 10;
+                        }
+                        mem[index] = ascii;
+                    }
+                    '[' => {
+                        match mem[index] {
+                            0 => parse_parentheses!(file, '[', ']', i),
+                            _ => ()
+                        }
+                    }
+                    ']' => {
+                        match mem[index] {
+                            0 => (),
+                            _ => parse_parentheses!(file, ']', '[', i)
+                        }
+                    }
                     _ => ()
                 }
+
+                i += 1;
             }
-            ']' => {
-                match mem[index] {
-                    0 => (),
-                    _ => parse_parentheses!(file, ']', '[', i)
-                }
-            }
-            _ => ()
         }
 
-        i += 1;
+        _ => panic!("ERROR: Not a valid brainfuck file.")
     }
 }
